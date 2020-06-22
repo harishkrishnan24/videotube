788e901757c2d0189aeb32076af016969f9dbec3
Harish Krishnan <harishkrishnan1993@gmail.com>
2020-06-22 10:08:00 +0530

    Create HTML layout

--- index.php
+++ index.php
@@ -1,1 +1,20 @@
-sandwiches
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Video Tube</title>
+        <link rel="stylesheet" type="text/css" href="assets/css/style.css">
+    </head>
+    <body>
+        <div id="pageContainer">
+            <div id="mastHeadContainer">
+                Header
+            </div>
+            <div id="sideNavContainer">
+                SideNav
+            </div>
+            <div id="mainSectionContainer">
+                MainSection
+            </div>
+        </div>
+    </body>
+</html>
